31355e8e2c57c8469b5602ae59638ade1d5ad6f8
Muhammad Raihan Molla <mdraihan2328@gmail.com>
2024-05-13 19:13:19 +0600

    Add files via upload

---
+++ Watermelon.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    string Output;
+    cin>>w;
+    if(w>2 && w%2==0)
+    {
+        cout<<"YES";
+    }
+    else
+    {
+        cout<<"NO";
+    }
+
+    return 0;
+}
