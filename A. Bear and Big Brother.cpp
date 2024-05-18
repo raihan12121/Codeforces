fd4bb09e5bbd1cd7d0bd0a5b2b5b8e8d00f510c2
Muhammad Raihan Molla <mdraihan2328@gmail.com>
2024-05-19 00:29:55 +0600

    Add files via upload

---
+++ A. Bear and Big Brother.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int a,b,i,c=0;
+    cin>>a>>b;
+   while(a<=b)
+    {
+        a*=3;
+        b*=2;
+        c++;
+        //if(a>b)
+
+
+    }
+    cout<<c<<endl;
+
+    return 0;
+}
